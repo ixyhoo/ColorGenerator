8e3e0ee66dd7365dbb586c0bcb8d0bf7da94e485
ixyhoo <ixyhoo@outlook.fr>
2023-05-31 13:48:38 +0200

    debug ok

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-
-let body  = document.querySelector("body")
-    body.addEventListener("click", function () {
-
     let num1 = Math.floor(Math.random() * 255);
     let num2 = Math.floor(Math.random() * 255);
     let num3 = Math.floor(Math.random() * 255);
-console.log(num1, num2, num3);
 
-    let rgbColor = `rgb(${num1}, ${num2}, ${num3})`; 
 
-body.style.backgroundColor = rgbColor;
+    let rgbColor = `rgb(${num1}, ${num2}, ${num3})`;
 
-});
+    document.body.style.backgroundColor = rgbColor;
+    document.body.innerHTML = `<h2>${rgbColor}</h2>`;
+
+
+
 
                                                          
